8f4c7e8f8ae6a0d2e7dcbdd52a0ca7410e0f2181
tcioffidinkel <tcioffidinkel@gmail.com>
2015-09-22 18:00:33 -0700

    Update MainActivity.java

--- app/src/main/java/com/caliware/stackapp/MainActivity.java
+++ app/src/main/java/com/caliware/stackapp/MainActivity.java
@@ -61,7 +61,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.exit) {
+			
+			finish();
+			
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
